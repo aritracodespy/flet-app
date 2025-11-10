b184d362314112ba1eeba651eb761ae89795d547
aritracodespy <aritracodespy@gmail.com>
2025-11-10 12:30:33 +0530

    Refactor note functions to use dynamic URL
    Refactor note handling to accept URL as a parameter and update credential management.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,7 @@
 import flet as ft
 import requests
-URL = "api-url"
 
-def get_notes(key, url=URL):
+def get_notes(key, url):
     payload = {
         "command": "select * from mysyncnotes;"
     }
@@ -24,15 +23,17 @@ def get_notes(key, url=URL):
     except:
         return None
 
-def add_note(note, key, url=URL):
+def add_note(note, key, url):
     headers = {
         "Content-Type": "application/json",
         "X-API-Key": key
     }
+
     payload = {
         "command":"INSERT INTO mysyncnotes (note) VALUES (?)",
         "params":[note]
     }
+
     try:
         r = requests.post(url=url, headers=headers, json=payload)
         r.raise_for_status()
@@ -40,49 +41,58 @@ def add_note(note, key, url=URL):
     except Exception as e:
         return {"error": str(e)}
     
-def delete_note(id, key, url=URL):
+def delete_note(id, key, url):
     headers = {
         "Content-Type": "application/json",
         "X-API-Key": key
     }
+
     payload = {
         "command":"DELETE FROM mysyncnotes WHERE id = ?",
         "params":[int(id)]
     }
+
     try:
         r = requests.post(url=url, headers=headers, json=payload)
         r.raise_for_status()
         return r.json()
     except Exception as e:
         return {"error": str(e)}
+
 
 def main(page: ft.Page):
     page.title = "SyncNotes"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     
-    def save_key(e):
-        apikey = e.control.value
-        if apikey is None: return
-        if apikey.strip() == "": return
-        page.client_storage.set("SYNC-NOTE-API-KEY", apikey)
+    def save_cred(e):
+        apikey = key_i.value
+        url = url_i.value
+        if apikey is None or url is None: return
+        if apikey.strip() == "" or url.strip() == "": return
+        page.client_storage.set("SYNC-NOTE-CRED", {"api-key": apikey, "url": url})
+        settings.visible=False
         page.open(ft.SnackBar(content=ft.Text(value="key saved!!")))
         render_ui()
 
-    def load_key():
-        return page.client_storage.get("SYNC-NOTE-API-KEY")
+    def load_cred():
+        cred = page.client_storage.get("SYNC-NOTE-CRED")
+        if cred is not None:
+            return cred
+        else:
+            return {"api-key": "", "url": ""}
 
 
 
     def yes_delete(e):
         id = dlg_modal.data
         if not id: return
-        key = load_key()
-        if key is None: 
-            page.open(ft.SnackBar(content=ft.Text(value="key is not set!")))
+        cred = load_cred()
+        if cred.get("api-key", "") == "" or cred.get("url", "") == "":
+            page.open(ft.SnackBar(content=ft.Text(value="Creadentials is not set yet!")))
             return
         page.close(dlg_modal)
-        r = delete_note(id=id, key=key)
+        r = delete_note(id=id, key=cred["api-key"], url=cred["url"])
         if "error" in r:
             page.open(ft.SnackBar(content=ft.Text(value=r.get("error", "Error!!"))))
         if r.get("success",False):
@@ -112,19 +122,36 @@ def main(page: ft.Page):
         multiline=False,
         password=True,
         can_reveal_password=True,
-        value=load_key(),
-        on_submit=save_key,
-        visible=False
+        value=load_cred()["api-key"]
     )
+    url_i = ft.TextField(
+        label="url",
+        multiline=False,
+        value=load_cred()["url"]
+    )
+
+    settings = ft.Column(controls=[
+        key_i, 
+        url_i,
+        ft.ElevatedButton(
+            text="SAVE", 
+            on_click=save_cred,
+            width=500,
+            bgcolor=ft.Colors.BLUE_500,
+            color=ft.Colors.BLACK
+        ),
+        ft.Divider(thickness=3)
+    ],visible=False)
+
     note_i = ft.TextField(label="Your new note",expand=True, multiline=True,min_lines=1,max_lines=5)
     note_list = ft.ListView(expand=True,auto_scroll=True,spacing=10)
 
     def render_ui():
-        apikey = load_key()
-        if apikey is None:
-            page.open(ft.SnackBar(content=ft.Text(value="key is not set!")))
+        cred = load_cred()
+        if cred.get("api-key", "") == "" or cred.get("url", "") == "":
+            page.open(ft.SnackBar(content=ft.Text(value="Creadentials is not set yet!")))
             return
-        notes = get_notes(key=apikey)
+        notes = get_notes(key=cred["api-key"],url=cred["url"])
         if notes is None:
             page.open(ft.SnackBar(content=ft.Text(value="Error")))
             return
@@ -151,10 +178,11 @@ def main(page: ft.Page):
 
     def send(e):
         message = note_i.value
-        key = key_i.value
-        if message is None or key is None: return
+        cred = load_cred()
+        if cred.get("api-key", "") == "" or cred.get("url", "") == "" or message is None:
+            return
         if message.strip() == "": return
-        r = add_note(note=message,key=key)
+        r = add_note(note=message,key=cred["api-key"], url=cred["url"])
 
         if "error" in r:
             page.open(ft.SnackBar(content=ft.Text(value=r.get("error", "Error!!"))))
@@ -162,8 +190,8 @@ def main(page: ft.Page):
             page.open(ft.SnackBar(content=ft.Text(value=r.get("message","added"))))
             note_i.value = ""
             render_ui()
-    def toggle_key(e):
-        key_i.visible = not key_i.visible
+    def toggle_settings(e):
+        settings.visible = not settings.visible
         page.update()
     page.add(
         ft.Container(
@@ -173,9 +201,10 @@ def main(page: ft.Page):
                 [
                     ft.Row(controls=[
                         ft.Text("SyncNotes",size=30),
-                        ft.IconButton(icon=ft.Icons.SETTINGS,on_click=toggle_key)
+                        ft.IconButton(icon=ft.Icons.SETTINGS,on_click=toggle_settings)
                     ],alignment=ft.MainAxisAlignment.SPACE_BETWEEN),
-                    key_i,
+                    ft.Divider(thickness=3),
+                    settings,
                     note_list,
                     ft.Row(
                         [
