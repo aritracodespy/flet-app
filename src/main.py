af2bb92ec12b2e83078d857ae1cfd567379980a2
aritracodespy <aritracodespy@gmail.com>
2025-11-29 20:37:59 +0530

    Refactor note handling and update UI components
    Refactor note management functions and improve UI.

--- src/main.py
+++ src/main.py
@@ -1,222 +1,189 @@
 import flet as ft
 import requests
+import json
+import time
+import base64
 
-def get_notes(key, url):
-    payload = {
-        "command": "select * from mysyncnotes;"
-    }
+def encode(original_string):
     try:
-        r = requests.post(
-            url=url,
-            headers={
-                "Content-Type": "application/json",
-                "X-API-Key": key
-            },
-            json=payload
+        string_bytes = original_string.encode('utf-8')
+        encoded_bytes = base64.b64encode(string_bytes)
+        encoded_string = encoded_bytes.decode('utf-8')
+        return encoded_string
+    except:
+        return original_string
+
+def decode(base64_string):
+    try:
+        base64_bytes = base64_string.encode('utf-8')
+        decoded_bytes = base64.b64decode(base64_bytes)
+        decoded_string = decoded_bytes.decode('utf-8')
+        return decoded_string
+    except:
+        return base64_string
+
+def _update_note_content(base_url, key, text):
+    url = base_url + "/db/my_notepad_app_content/1"
+    headers = {"Content-Type": "application/json", "Authorization": key}
+    note = encode(text)
+
+    try:
+        r = requests.put(
+            url = url,
+            headers = headers,
+            json = {"note-content": note}
         )
         r.raise_for_status()
-        data = r.json()
-        if data["success"] and data["data"] is not None:
-            return data["data"]
+        return r.json()
+    except:
+        return None
+
+
+def _load_note_content(base_url, key):
+    url = base_url + "/db/my_notepad_app_content/1"
+    headers = {"Content-Type": "application/json", "Authorization": key}
+    try:
+        r = requests.get(url=url, headers=headers)
+        r.raise_for_status()
+        d = r.json()
+        if d.get("ok",False):
+            return decode(d["result"]["note-content"])
         else:
             return None
     except:
         return None
 
-def add_note(note, key, url):
-    headers = {
-        "Content-Type": "application/json",
-        "X-API-Key": key
-    }
-
-    payload = {
-        "command":"INSERT INTO mysyncnotes (note) VALUES (?)",
-        "params":[note]
-    }
-
-    try:
-        r = requests.post(url=url, headers=headers, json=payload)
-        r.raise_for_status()
-        return r.json()
-    except Exception as e:
-        return {"error": str(e)}
-    
-def delete_note(id, key, url):
-    headers = {
-        "Content-Type": "application/json",
-        "X-API-Key": key
-    }
-
-    payload = {
-        "command":"DELETE FROM mysyncnotes WHERE id = ?",
-        "params":[int(id)]
-    }
-
-    try:
-        r = requests.post(url=url, headers=headers, json=payload)
-        r.raise_for_status()
-        return r.json()
-    except Exception as e:
-        return {"error": str(e)}
-
 
 def main(page: ft.Page):
-    page.title = "SyncNotes"
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
-    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-    
-    def save_cred(e):
-        apikey = key_i.value
-        url = url_i.value
-        if apikey is None or url is None: return
-        if apikey.strip() == "" or url.strip() == "": return
-        page.client_storage.set("SYNC-NOTE-CRED", {"api-key": apikey, "url": url})
-        settings.visible=False
-        page.open(ft.SnackBar(content=ft.Text(value="key saved!!")))
-        render_ui()
+    page.title = "My Notepad"
+    page.scroll = True
 
     def load_cred():
-        cred = page.client_storage.get("SYNC-NOTE-CRED")
-        if cred is not None:
-            return cred
-        else:
-            return {"api-key": "", "url": ""}
+        s = page.client_storage.get("SYNC-NOTE-CRED")
+        return json.loads(s) if s else {"api-key": "", "url": ""}
 
-
-
-    def yes_delete(e):
-        id = dlg_modal.data
-        if not id: return
-        cred = load_cred()
-        if cred.get("api-key", "") == "" or cred.get("url", "") == "":
-            page.open(ft.SnackBar(content=ft.Text(value="Creadentials is not set yet!")))
-            return
-        page.close(dlg_modal)
-        r = delete_note(id=id, key=cred["api-key"], url=cred["url"])
-        if "error" in r:
-            page.open(ft.SnackBar(content=ft.Text(value=r.get("error", "Error!!"))))
-        if r.get("success",False):
-            page.open(ft.SnackBar(content=ft.Text(value=r.get("message","deleted"))))
-            render_ui()
-
-
-    dlg_modal = ft.AlertDialog(
-        modal=True,
-        title=ft.Text("Please confirm"),
-        content=ft.Text("Do you really want to delete this note?"),
-        actions=[
-            ft.TextButton("Yes", on_click=yes_delete),
-            ft.TextButton("No", on_click=lambda _: page.close(dlg_modal)),
-        ],
-        actions_alignment=ft.MainAxisAlignment.END,
-    )
-
-    def open_dlg(note_id):
-        dlg_modal.data = note_id
-        page.open(dlg_modal)
-        
+    creds = load_cred()
 
     key_i = ft.TextField(
-        label="secret key",
-        expand=False,
-        multiline=False,
+        label="Secret Key",
         password=True,
         can_reveal_password=True,
-        value=load_cred()["api-key"]
+        value=creds["api-key"],
     )
+
     url_i = ft.TextField(
-        label="url",
-        multiline=False,
-        value=load_cred()["url"]
+        label="Backend URL",
+        value=creds["url"],
     )
 
-    settings = ft.Column(controls=[
-        key_i, 
-        url_i,
-        ft.ElevatedButton(
-            text="SAVE", 
-            on_click=save_cred,
-            width=500,
-            bgcolor=ft.Colors.BLUE_500,
-            color=ft.Colors.BLACK
-        ),
-        ft.Divider(thickness=3)
-    ],visible=False)
+    settings = ft.Column(visible=False)
 
-    note_i = ft.TextField(label="Your new note",expand=True, multiline=True,min_lines=1,max_lines=5)
-    note_list = ft.ListView(expand=True,auto_scroll=True,spacing=10)
+    def save_cred(e):
+        apikey = key_i.value.strip()
+        url = url_i.value.strip()
 
-    def render_ui():
-        cred = load_cred()
-        if cred.get("api-key", "") == "" or cred.get("url", "") == "":
-            page.open(ft.SnackBar(content=ft.Text(value="Creadentials is not set yet!")))
+        if not apikey or not url:
             return
-        notes = get_notes(key=cred["api-key"],url=cred["url"])
-        if notes is None:
-            page.open(ft.SnackBar(content=ft.Text(value="Error")))
+
+        page.client_storage.set(
+            "SYNC-NOTE-CRED",
+            json.dumps({"api-key": apikey, "url": url}),
+        )
+
+        settings.visible = False
+        page.open(ft.SnackBar(content=ft.Text("Credentials Saved!")))
+        page.update()
+        _read()
+
+    save_btn = ft.ElevatedButton(
+        text="SAVE",
+        width=500,
+        bgcolor=ft.Colors.BLUE_500,
+        color=ft.Colors.WHITE,
+        on_click=save_cred,
+    )
+
+    settings.controls = [key_i, url_i, save_btn, ft.Divider(thickness=3)]
+
+    main_textfield = ft.TextField(
+        multiline=True,
+        border=ft.InputBorder.NONE,
+        hint_text="Your thoughts...",
+    )
+
+    def _read():
+        creds = load_cred()
+        base_url = creds["url"]
+        key = creds["api-key"]
+
+        if not base_url or not key:
             return
-        note_list.controls.clear()
-        if len(notes) == 0:
-            note_list.controls.append(ft.Text(value="No saved notes"))
-        for note in reversed(notes): # type: ignore
-            content = ft.Card(
-                content=ft.Container(
-                    padding=10,
-                    content=ft.Row(
-                        controls=[
-                            ft.Text(note["note"], expand=True,selectable=True),
-                            ft.IconButton(
-                                icon=ft.Icons.DELETE_OUTLINE,
-                                on_click=lambda e, id=note["id"]: open_dlg(note_id=id),
-                            ),
-                        ]
-                    )
-                )
-            )
-            note_list.controls.append(content)
+
+        data = _load_note_content(base_url, key)
+
+        if data is None:
+            page.open(ft.SnackBar(content=ft.Text("Can't load notes")))
+            return
+
+        main_textfield.value = data
         page.update()
 
-    def send(e):
-        message = note_i.value
-        cred = load_cred()
-        if cred.get("api-key", "") == "" or cred.get("url", "") == "" or message is None:
-            return
-        if message.strip() == "": return
-        r = add_note(note=message,key=cred["api-key"], url=cred["url"])
-
-        if "error" in r:
-            page.open(ft.SnackBar(content=ft.Text(value=r.get("error", "Error!!"))))
-        if r.get("success",False):
-            page.open(ft.SnackBar(content=ft.Text(value=r.get("message","added"))))
-            note_i.value = ""
-            render_ui()
     def toggle_settings(e):
         settings.visible = not settings.visible
         page.update()
+
+    def _save(e = None):
+        text = main_textfield.value.strip()
+        if not text:
+            return
+
+        creds = load_cred()
+        base_url = creds["url"]
+        key = creds["api-key"]
+
+        if not base_url or not key:
+            page.open(ft.SnackBar(content=ft.Text("Credentials not saved!")))
+            return
+
+        data = _update_note_content(base_url, key, text)
+
+        if data is None:
+            page.open(ft.SnackBar(content=ft.Text("Can't save notes")))
+            return
+
+        if data.get("ok", False):
+            page.open(ft.SnackBar(content=ft.Text("Notes Saved!")))
+        else:
+            page.open(ft.SnackBar(content=ft.Text(str(data.get("error")))))
+
+    page.floating_action_button = ft.PopupMenuButton(
+        items=[
+            ft.PopupMenuItem(icon=ft.Icons.SAVE, text="Save", on_click=_save),
+            ft.PopupMenuItem(icon=ft.Icons.SETTINGS, text="Settings", on_click=toggle_settings),
+        ]
+    )
+
     page.add(
         ft.Container(
             expand=True,
-            padding=ft.Padding(0,20,0,10),
+            padding=ft.Padding(0, 20, 0, 10),
             content=ft.Column(
-                [
-                    ft.Row(controls=[
-                        ft.Text("SyncNotes",size=30),
-                        ft.IconButton(icon=ft.Icons.SETTINGS,on_click=toggle_settings)
-                    ],alignment=ft.MainAxisAlignment.SPACE_BETWEEN),
-                    ft.Divider(thickness=3),
-                    settings,
-                    note_list,
-                    ft.Row(
-                        [
-                            note_i,
-                            ft.IconButton(icon=ft.Icons.NOTE_ADD_OUTLINED, on_click=send, tooltip="Add Note"),
-                        ],
-                    ),
-                ],
                 expand=True,
+                controls=[settings, main_textfield],
             ),
         )
     )
 
-    render_ui()
+    if not creds["url"] or not creds["api-key"]:
+        settings.visible = True
+        page.update()
+    else:
+        _read()
+
+    def auto_save():
+        time.sleep(60)
+        _save()
+
+    page.run_thread(auto_save)
+
 ft.app(target=main)
